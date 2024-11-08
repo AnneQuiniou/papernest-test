409ebac77ab23a2ee88caacaafc6192f408f6798
Anne <annejeannequiniou@gmail.com>
2024-11-08 19:36:17 +0100

    feature(layout): update default layout

--- src/app/templates/default-layout/default-layout.component.ts
+++ src/app/templates/default-layout/default-layout.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
-import { MenuComponent } from "../../menu/menu/menu.component";
+import { Component, inject } from '@angular/core';
+import { MenuComponent } from '../../menu/menu/menu.component';
 import { RouterOutlet } from '@angular/router';
+import { CounterService } from '../../services/counter/counter.service';
+import { AsyncPipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-default-layout',
   standalone: true,
-  imports: [MenuComponent, RouterOutlet],
+  imports: [MenuComponent, RouterOutlet, AsyncPipe, NgClass],
   templateUrl: './default-layout.component.html',
-  styleUrl: './default-layout.component.scss'
+  styleUrl: './default-layout.component.scss',
 })
 export class DefaultLayoutComponent {
-
+  private readonly counterService = inject(CounterService);
+  count$ = this.counterService.getCount();
 }
