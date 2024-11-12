921b8a5d4f7da4e946dc43d54149038b09c49dc3
Anne <annejeannequiniou@gmail.com>
2024-11-12 19:24:46 +0100

    feature(storage-service): document methods

--- src/app/services/storage/storage.service.ts
+++ src/app/services/storage/storage.service.ts
@@ -10,10 +10,26 @@ export type StorageKey = keyof StorageSchema;
   providedIn: 'root',
 })
 export class StorageService {
+  /**
+   * Write a key value pair to the session storage
+   *
+   * @template T
+   * @param {T} key the key value for the session storage
+   * @param {StorageSchema[T]} value the value to write in the session storage
+   * @memberof StorageService
+   */
   write<T extends StorageKey>(key: T, value: StorageSchema[T]): void {
     window['sessionStorage'].setItem(key, JSON.stringify(value));
   }
 
+  /**
+   * Base on key provided, return parsed corresponding value in session storage or null
+   *
+   * @template T
+   * @param {T} key
+   * @return {*}  {(StorageSchema[T] | null)}
+   * @memberof StorageService
+   */
   read<T extends StorageKey>(key: T): StorageSchema[T] | null {
     const value = window['sessionStorage'].getItem(key);
     if (value === null) {
@@ -22,6 +38,11 @@ export class StorageService {
     return JSON.parse(value) as StorageSchema[T];
   }
 
+  /**
+   * Remove stored key value pair from session storage
+   *
+   * @param {StorageKey} key
+   */
   remove(key: StorageKey): void {
     window['sessionStorage'].removeItem(key);
   }
