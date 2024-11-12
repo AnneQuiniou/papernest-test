261b6fbc2e1788a6541139dccde319c19c185053
Anne <annejeannequiniou@gmail.com>
2024-11-12 11:37:05 +0100

    fix(testing): add age form testing

--- src/app/forms/age-verification-form/age-verification-form.component.spec.ts
+++ src/app/forms/age-verification-form/age-verification-form.component.spec.ts
@@ -17,7 +17,72 @@ describe('AgeVerificationFormComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should be invalid if day value is above 31', async () => {
+    const birthDayValue = fixture.nativeElement.querySelector(
+      'input[formControlName="birthDay"]',
+    );
+    birthDayValue.value = '45';
+
+    birthDayValue.dispatchEvent(new Event('input'));
+    await fixture.whenStable();
+    fixture.detectChanges();
+    expect(component.form.valid).toBeFalse();
+  });
+
+  it('should be invalid if day value is empty', async () => {
+    const birthDayValue = fixture.nativeElement.querySelector(
+      'input[formControlName="birthDay"]',
+    );
+    birthDayValue.value = '';
+
+    birthDayValue.dispatchEvent(new Event('input'));
+    await fixture.whenStable();
+    fixture.detectChanges();
+    expect(component.form.valid).toBeFalse();
+  });
+
+  it('should be invalid if all values entered but not over 18', async () => {
+    const underAgeYear = new Date().getFullYear() - 15;
+    const birthDayValue = fixture.nativeElement.querySelector(
+      'input[formControlName="birthDay"]',
+    );
+    const birthMonthValue = fixture.nativeElement.querySelector(
+      'input[formControlName="birthMonth"]',
+    );
+    const birthYearValue = fixture.nativeElement.querySelector(
+      'input[formControlName="birthYear"]',
+    );
+    birthDayValue.value = '03';
+    birthMonthValue.value = '11';
+    birthYearValue.value = underAgeYear.toString();
+
+    birthDayValue.dispatchEvent(new Event('input'));
+    birthMonthValue.dispatchEvent(new Event('input'));
+    birthYearValue.dispatchEvent(new Event('input'));
+    await fixture.whenStable();
+    fixture.detectChanges();
+    expect(component.form.valid).toBeFalse();
+  });
+
+  it('should be valid if all values entered and within range', async () => {
+    const birthDayValue = fixture.nativeElement.querySelector(
+      'input[formControlName="birthDay"]',
+    );
+    const birthMonthValue = fixture.nativeElement.querySelector(
+      'input[formControlName="birthMonth"]',
+    );
+    const birthYearValue = fixture.nativeElement.querySelector(
+      'input[formControlName="birthYear"]',
+    );
+    birthDayValue.value = '03';
+    birthMonthValue.value = '11';
+    birthYearValue.value = '1988';
+
+    birthDayValue.dispatchEvent(new Event('input'));
+    birthMonthValue.dispatchEvent(new Event('input'));
+    birthYearValue.dispatchEvent(new Event('input'));
+    await fixture.whenStable();
+    fixture.detectChanges();
+    expect(component.form.valid).toBeTrue();
   });
 });
